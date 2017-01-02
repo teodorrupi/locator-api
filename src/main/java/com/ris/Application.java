d361200d7762b7db063b85710eb2ec6a3bf65d09
Teodor Rupi <teodorrupi@gmail.com>
2017-01-02 19:51:52 +0100

    add tomcat exclusion

--- src/main/java/com/ris/Application.java
+++ src/main/java/com/ris/Application.java
@@ -2,10 +2,13 @@
 package com.ris;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
